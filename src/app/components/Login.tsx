7298123d10159841cd1050269dbc2e428b548645
Aditya <yaditya@deloitte.com>
2024-01-10 09:47:11 +0530

    Better error handling and JWT token generation

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -19,40 +19,42 @@ const LoginComponent = () => {
   const [error, setError] = React.useState<string>("");
 
   const login = async (user: any) => {
-  console.log(`Login function called with values ${JSON.stringify(user)}`)
-  const url = "http://localhost:3001/login";
+    console.log(`Login function called with values ${JSON.stringify(user)}`);
+    const url = "http://localhost:3001/login";
     try {
       const response = await axios.post(url, user);
-      console.log(`${JSON.stringify(response.data)}`);
+      console.log(`JWT token: ${JSON.stringify(response.data)}`);
       setResult(response.data.result);
       setError("");
+      /* console.log(`JWT token: ${response.data.result}`) */
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        setError(error.message);
-        setResult("");
-        console.log(error);
+      if (axios.isAxiosError(error) && error.response) {
+        // Check for response status code and set a custom error message
+        if (error.response.status === 401) {
+          setError("Password incorrect");
+        } else {
+          setError(error.response.data.message || "An error occurred");
+        }
       } else {
-        console.log(error);
+        setError("Network Error");
       }
+      setResult("");
+      console.log(error);
     }
-  }
-  
+  };
+
   const formik = useFormik({
     initialValues: {
-     
       email: "",
       password: "",
-      
     },
     validationSchema: Yup.object({
-      
       email: Yup.string()
         .email("Invalid email address")
         .required("Email is required"),
       password: Yup.string()
         .min(6, "password must be 6 characters or more")
         .required("Password is required"),
-      
     }),
     onSubmit: async (values) => {
       // setTimeout(() => {
@@ -65,70 +67,74 @@ const LoginComponent = () => {
   return (
     <React.Fragment>
       <form onSubmit={formik.handleSubmit}>
-      <Container>
-        <Grid container spacing={2} pt={2} px={25}>
-          <Grid item xs={12}>
-            <Typography variant="h4">Login</Typography>
-          </Grid>
-          {result && (
-            <Grid item xs={12} color={"green"}>
-              <Typography variant="h6">{result}</Typography>
+        <Container>
+          <Grid container spacing={2} pt={2} px={25}>
+            <Grid item xs={12}>
+              <Typography variant="h4">Login</Typography>
             </Grid>
-          )}
-          {error && (
-            <Grid item xs={12} color={"red"}>
-              <Typography variant="h6">{error}</Typography>
-            </Grid>
-          )}
+            {result && (
+              <Grid item xs={12} color={"green"}>
+                <Typography variant="h6">
+                  {/* {result} */} Token Generated!
+                </Typography>
+              </Grid>
+            )}
+            {error && (
+              <Grid item xs={12} color={"red"}>
+                <Typography variant="h6">{error}</Typography>
+              </Grid>
+            )}
 
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              id="email"
-              name="email"
-              label="Email"
-              variant="outlined"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              error={Boolean(formik.errors.email)}
-              helperText={formik.errors.email}
-            />
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="email"
+                name="email"
+                label="Email"
+                variant="outlined"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={Boolean(formik.errors.email)}
+                helperText={formik.errors.email}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="password"
+                name="password"
+                label="Password"
+                variant="outlined"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={Boolean(formik.errors.password)}
+                helperText={formik.errors.password}
+              />
+              {/* {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>} */}
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                fullWidth
+                type="submit"
+                disabled={!formik.isValid || formik.isSubmitting}
+              >
+                Login
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              id="password"
-              name="password"
-              label="Password"
-              variant="outlined"
-              type="password"
-              value={formik.values.password}
-              onChange={formik.handleChange}
-              error={Boolean(formik.errors.password)}
-              helperText={formik.errors.password}
-            />
-            {/* {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>} */}
+          <Grid container spacing={2} pt={2} px={25}>
+            <Grid item xs={2.75}>
+              <Typography>Don't have an account?</Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Link href="/register" underline="hover">
+                {"Register"}
+              </Link>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Button variant="contained"
-              fullWidth
-              type="submit"
-              disabled={!formik.isValid || formik.isSubmitting}>
-              Login
-            </Button>
-          </Grid>
-        </Grid>
-        <Grid container spacing={2} pt={2} px={25}>
-          <Grid item xs={2.75}>
-            <Typography>Don't have an account?</Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <Link href="/register" underline="hover">
-              {"Register"}
-            </Link>
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
       </form>
     </React.Fragment>
   );
